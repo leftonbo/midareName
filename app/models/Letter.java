e9b541868a0b33f9f06dcaf7cc3e909ba77677ab
seameteo <dragonbird.tonbo@gmail.com>
2014-09-04 18:16:22 +0900

    Letterの変数を定義しておく

--- app/models/Letter.java
+++ app/models/Letter.java
@@ -1,5 +1,51 @@
 package models;
 
-public class Letter {
+import java.util.*;
 
+import javax.persistence.*;
+
+import play.db.ebean.*;
+import play.db.ebean.Model.Finder;
+import play.data.validation.Constraints;
+
+@Entity
+public class Letter extends Model {
+
+	/**
+	 * SUID かな？
+	 */
+	private static final long serialVersionUID = -3229399137347456774L;
+
+	@Id
+	public long id;			// ID
+
+    @Constraints.Required
+	public String letter;	// Letter
+
+    @Constraints.Required
+	public String next;		// Next Letter
+
+    @Constraints.Required
+    @Constraints.Min(1)
+	public int numLimit;
+    
+    /**
+     * Define default values
+     */
+    public Letter() {
+    	numLimit = 2;
+    }
+    
+    // ==========
+
+	/**
+	 * The Finder
+	 */
+    public static final Finder<Long,Letter> find =
+    	new Finder<Long,Letter>(Long.class, Letter.class);
+
+	public static final List<Letter> all() {
+		return find.all();
+	}
+	
 }
