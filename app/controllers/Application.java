ef851d0ed6100ab40a156955bfa14943dfa4363c
seameteo <dragonbird.tonbo@gmail.com>
2014-09-05 13:39:39 +0900

    route定義その2

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -11,4 +11,23 @@ public class Application extends Controller {
         return ok(index.render("Your new application is ready."));
     }
 
+    public static Result letterList() {
+        return TODO;
+    }
+    public static Result letterNew() {
+        return TODO;
+    }
+    public static Result letterWriteNew() {
+        return TODO;
+    }
+    public static Result letterSelectList(long id) {
+        return TODO;
+    }
+    
+    public static Result letterCarryEdit(long id) {
+        return TODO;
+    }
+    public static Result letterCarryWrite(long id) {
+        return TODO;
+    }
 }
